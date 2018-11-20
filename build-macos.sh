74e7e660d1e4e9481c7eac7abcc0be9a0d92545d
shwld <shu.account@outlook.com>
2018-11-21 00:02:48 +0900

    initial commit

---
+++ build-macos.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+APP="GMail.app"
+mkdir -p $APP/Contents/{MacOS,Resources}
+go build -o $APP/Contents/MacOS/gMail
+cat > $APP/Contents/Info.plist << EOF
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+	<key>CFBundleExecutable</key>
+	<string>gMail</string>
+	<key>CFBundleIconFile</key>
+	<string>icon.icns</string>
+	<key>CFBundleIdentifier</key>
+	<string>net.shwld.gmail-client</string>
+</dict>
+</plist>
+EOF
+cp icon.icns $APP/Contents/Resources/icon.icns
+find $APP
